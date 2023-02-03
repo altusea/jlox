c87377419a53949374c7169054a01534cc764b88
altusea <zt.song@outlook.com>
2023-02-03 15:53:05 +0800

    Initial commit.

---
+++ src/main/java/com/craftinginterpreters/lox/Token.java
@@ -0,0 +1,9 @@
+package com.craftinginterpreters.lox;
+
+record Token(TokenType type, String lexeme, Object literal, int line) {
+
+  @Override
+  public String toString() {
+    return type + " " + lexeme + " " + literal;
+  }
+}
