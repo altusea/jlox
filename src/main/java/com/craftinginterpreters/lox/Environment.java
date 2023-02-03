f878637762f7d725e950200ebfdb5301ca38ea99
altusea <zt.song@outlook.com>
2023-02-03 16:02:07 +0800

    reformat code

--- src/main/java/com/craftinginterpreters/lox/Environment.java
+++ src/main/java/com/craftinginterpreters/lox/Environment.java
@@ -39,7 +39,7 @@ class Environment {
       return;
     }
 
-    throw new RuntimeError(name,        "Undefined variable '" + name.lexeme() + "'.");
+    throw new RuntimeError(name, "Undefined variable '" + name.lexeme() + "'.");
   }
 
   void define(String name, Object value) {
